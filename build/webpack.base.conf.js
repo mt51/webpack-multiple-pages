7faa40e116ecc321f4d626d4e90f6758522ba933
liuziyang <ziyang.liu@roadshare.com>
2018-01-16 17:51:46 +0800

    更新打包配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,6 +32,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter'),
+          emitWarning: !config.dev.showEslintErrorsInOverlay
+        }
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
